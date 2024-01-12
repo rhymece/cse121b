a11bd639b1e8b984126b97a3c983e9cd50075d4f
rhymece <rhymece@gmail.com>
2024-01-12 11:19:10 -0500

    adding JS day 1

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -3,18 +3,21 @@
 /* Step 1 - Setup type tasks - no code required */
 
 /* Step 2 - Variables */
+let fullname = "Rhym Cagbay";
+let currentYear = "2024";
 
-
+let profilePicture = 'images/rhym.png';
 
 
 /* Step 3 - Element Variables */
-
-
-
-
+const nameElement = document.getElementById("name");
+const foodElement = document.getElementById("name");
+let yearElement = document.querySelector("#year");
+let imageElement = document.querySelector("img");
 
 /* Step 4 - Adding Content */
-
+nameElement.innerHTML = `<strong>${fullname}</strong>`;
+imageElement.setAttribute("src", profilePicture);
 
 
 
